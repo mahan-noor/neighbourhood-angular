aa428522176c541750f30b1deee4ca47f667e855
mahan-noor <ismahanabey@gmail.com>
2021-06-07 06:20:23 +0300

    generate component profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
 import { BusinessComponent } from './components/business/business.component';
 import { NeighborhoodComponent } from './components/neighborhood/neighborhood.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     BusinessComponent,
-    NeighborhoodComponent
+    NeighborhoodComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
