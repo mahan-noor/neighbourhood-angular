acab25c06c23a3922240e5096443e9d11c9ad482
mahan-noor <ismahanabey@gmail.com>
2021-06-08 08:24:06 +0300

    add services of the components and add a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BusinessComponent } from './components/business/business.component';
 import { NeighborhoodComponent } from './components/neighborhood/neighborhood.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BusinessComponent,
     NeighborhoodComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
